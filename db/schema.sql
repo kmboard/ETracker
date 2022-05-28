3c986d27f96ef461743e9f1b53db43e9d1d06584
Kija <kmboard1@yahoo.com>
2022-05-28 10:17:46 -0400

    fixed syntax errors

--- db/schema.sql
+++ db/schema.sql
@@ -5,25 +5,25 @@ USE employee_db;
 
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name  VARCHAR(30) INT NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE `role` (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30),
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT
-  FOREIGN KEY department_id
+  department_id INT,
+  FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT DEFAULT NULL
-  FOREIGN KEY (role_id) REFERENCES `role`(id) ON DELETE SET NULL
+  manager_id INT DEFAULT NULL,
+  FOREIGN KEY (role_id) REFERENCES `role`(id) ON DELETE SET NULL,
   FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const mysql = require ('mysql2');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const db = mysql.createConnection(
@@ -15,69 +16,83 @@ const db = mysql.createConnection(
         password: 'Atlanta13!',
         database: 'employee_db'
     },
-    console.log('connected to the employee_db database. ')   
+    console.log('connected to the employee_db database. ')
 );
+    db.connect(function (err) {
+        if (err) throw err;
+      mainMenu();
+    });
 
-// const inquirer = require('inquirer')
+
+
 
 function mainMenu() {
     inquirer.prompt([
         {
-        type:'list',
-        message: 'What would you like to do',
-        name: 'user',
-        choices: ['View All Employees', 'Add Employees', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Deparetents', 'Add Departments', 'Quit']
+            type: 'list',
+            message: 'What would you like to do',
+            name: 'user',
+            choices: [
+                'View Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View Deparetent',
+                'Add Department',
+                'Quit']
         },
     ])
         .then((answers) => {
-            if(answers.user === "View All Employees") {
+            if (answers.user === "View Employees") {
                 viewEmployees();
             }
 
-            if(answers.user === "Add Employee") {
+            if (answers.user === "Add Employee") {
                 addEmployee();
+
             }
 
-            if(answers.user === "Update Employee Role") {
+            if (answers.user === "Update Role") {
                 updateRole();
             }
 
-            if(answers.user === "View All Roles") {
-                viewRoles();
+            if (answers.user === "View All Roles") {
+                viewRole();
             }
 
-            if(answers.user === "Add A Role") {
+            if (answers.user === "Add Role") {
                 addRole();
             }
 
-            if(answers.user === "View Departments") {
-                viewDepartments();
+            if (answers.user === "View Departments") {
+                viewDepartment();
             }
 
-            if(answers.user === "Add Department") {
+            if (answers.user === "Add Department") {
                 addDepartment();
             }
 
-            if(answers.user === "Quit") {
+            if (answers.user === "Quit") {
                 db.end();
             };
         });
-};
 
+};
 
 viewEmployees = () => {
     const sql = `Select employee.id, employee.first_name, employee.last_name, role.title, department.department_name, role.salary CONCAT (manager.first_name, "", manager.last_name) manager
         FROM employee
         LEFT JOIN employee.role.id=role.id
         LEFT JOIN department ON role.department_id=department.id
-        LEFT JOIN employee manager ON employee.manager_id=manager.id` 
+        LEFT JOIN employee manager ON employee.manager_id=manager.id`
     db.query(sql, (err, res) => {
         if (err) throw err;
         mainMenu();
     });
 };
 
-viewDepartments = () => {
+viewDepartment = () => {
     const sql = `SELECT * FROM department`;
     db.query(sql, (err, res) => {
         if (err) throw err;
@@ -85,7 +100,7 @@ viewDepartments = () => {
     });
 };
 
-viewRoles = () => {
+viewRole = () => {
     const sql = `SELECT * FROM role`;
     db.query(sql, (err, res) => {
         if (err) throw err;
@@ -95,33 +110,33 @@ viewRoles = () => {
 
 addDepartment = () => {
     inquirer.prompt([
-       {
-        type: 'input',
-        name: 'department',
-        message: "Whats department are you in?",
-       } 
-    ]) 
-    .then(answer => {
-        const sql = `INSERT INTO department (name) VALUES (?)`;
-        db.query(sql, answer.department, (err, res) => {
-            if (err) throw err;
-            mainMenu()
+        {
+            type: 'input',
+            name: 'department',
+            message: "Whats department are you in?",
+        }
+    ])
+        .then(answer => {
+            const sql = `INSERT INTO department (name) VALUES (?)`;
+            db.query(sql, answer.department, (err, res) => {
+                if (err) throw err;
+                mainMenu()
+            });
         });
-    });
 };
 
 addEmployee = () => {
     inquirer.prompt([
         {
-         type: 'input',
-         name: 'employee',
-         message: "Whats employee's name?",
-        } 
-     ]) 
+            type: 'input',
+            name: 'employee',
+            message: "Whats employee's name?",
+        }
+    ])
 };
 
 addRole = () => {
-    inquirer.prompt ([
+    inquirer.prompt([
         {
             type: 'input',
             name: 'role',
@@ -131,6 +146,58 @@ addRole = () => {
             type: 'input',
             name: 'salary',
             message: "Whats the salary of this role?"
-        }
+        },
     ])
 };
+
+updateRole = () => {
+    const sql = `SELECT *  FROM employee`;
+    db.query(sql, (err, res) => {
+        if (err) throw err;
+        const employees = results.map(({ first_name, last_name, id }) =>
+            ({ name: `${first_name} ${last_name}`, value: id }));
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Which employee's role would you lnoike to update?",
+                choices: employees
+            }
+        ])
+            .then(employeeChoice => {
+                const employee = employeeChoice.employee
+                const params = [];
+                params.push(employee);
+                const sql = `SELECT * FROM role`;
+                db.query(sql, (err, res) => {
+                    if (err) throw err;
+                    const roles = results.map(({ id, title }) => ({ name: title, value: id }));
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "role",
+                            message: "What is the new role of the Employee?",
+                            choices: roles
+                        }
+                    ])
+                        .then(roleAnswer => {
+                            const role = roleAnswer.role;
+                            params.unshift(role);
+                            const sql = `UPDATE employees
+                        SET role_id = ?
+                     WHERE id = ?`
+                            db.query(sql, params, (err) => {
+                                if (err) { throw err; }
+                                return viewEmployees();
+                            })
+                        })
+
+                })
+
+            })
+    })
+
+};
+
+
+mainMenu();
